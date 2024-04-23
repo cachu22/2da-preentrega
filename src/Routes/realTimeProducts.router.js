fff689371458ff3b4fc21abf255453d98fdcfbd9
AdriánFernández <adrianfer_87@hotmail.com>
2024-04-23 10:27:31 -0300

    Websockets + Handlebars2.0

--- src/Routes/realTimeProducts.router.js
+++ src/Routes/realTimeProducts.router.js
@@ -1,7 +1,7 @@
-import { Router } from "express"
-    export const router = Router()
+// import { Router } from "express"
+//     export const router = Router()
     
-    //realTimeProducts para ver los productos en tiempo real
-    router.get('/realtimeproducts', (req, res) => {
-        res.render('realTimeProducts', {})
-    })
+//     //realTimeProducts para ver los productos en tiempo real
+//     router.get('/realtimeproducts', (req, res) => {
+//         res.render('realTimeProducts', {})
+//     })
